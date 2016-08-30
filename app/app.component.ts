3ff70344120c65996c05d7e040568c710c1311df
Peter Vliet <plvliet@us.ibm.com>
2016-08-30 09:23:31 -0700

    add HTTP call to get json

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 ﻿import {Component} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/Rx';
 import {ProductListComponent} from './products/product-list.component';
 import {ProductService} from './products/product.service';
 
@@ -11,7 +12,8 @@ import {ProductService} from './products/product.service';
     </div>
     `,
     directives: [ProductListComponent],
-    providers: [ProductService]
+    providers: [ProductService,
+                HTTP_PROVIDERS]
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
